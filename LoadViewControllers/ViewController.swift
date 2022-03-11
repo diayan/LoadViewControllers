1035650ab97c65841bd44ea2544b714b607f35ed
diayan <diayans@outlook.com>
2022-03-11 12:58:30 +0000

    Initial Commit

---
+++ LoadViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoadViewControllers
+//
+//  Created by diayan siat on 11/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
