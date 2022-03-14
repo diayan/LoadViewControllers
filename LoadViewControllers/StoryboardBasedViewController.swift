d787a8dfcb45b1bd3de38ad619bc6d086402ba20
diayan <diayans@outlook.com>
2022-03-14 01:33:53 +0000

    create a new view controller to be for testing

---
+++ LoadViewControllers/StoryboardBasedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StoryboardBasedViewController.swift
+//  LoadViewControllers
+//
+//  Created by diayan siat on 14/03/2022.
+//
+
+import UIKit
+
+class StoryboardBasedViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
