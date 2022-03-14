301ee3e0b602a6f0a4b0d6419f2c54af26b4c22c
diayan <diayans@outlook.com>
2022-03-14 01:54:44 +0000

    completed loading view controllers from storyboard with outlet connections

--- StoryboardBasedViewControllerTests/StoryboardBasedViewControllerTests.swift
+++ StoryboardBasedViewControllerTests/StoryboardBasedViewControllerTests.swift
@@ -8,12 +8,22 @@
 import XCTest
 @testable import LoadViewControllers
 
+
+/*Run the tests to watch this pass. This demonstrates that a unit test can load a specific
+ view controller from a storyboard, with outlets connected */
+
 class StoryboardBasedViewControllerTests: XCTestCase {
+    
     func test_loading() {
         //Loads the main storyboard
         let sb = UIStoryboard(name: "Main", bundle: nil)
         
         //the identifier can be any unique name
-        let sut = sb.instantiateViewController(withIdentifier: String(describing: StoryboardBasedViewController.self))
+        let sut: StoryboardBasedViewController = sb.instantiateViewController(identifier: String(describing: StoryboardBasedViewController.self))
+        
+        //this loads the VC's view from storyboard including outlet connections
+        sut.loadViewIfNeeded()
+        
+        XCTAssertNotNil(sut.label)
     }
 }
