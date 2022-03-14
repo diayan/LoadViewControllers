5ed03c2e6826d2f92fd9778bc13bbe1919d0a4f6
diayan <diayans@outlook.com>
2022-03-14 02:32:09 +0000

    setup code based view controller

---
+++ LoadViewControllers/CodeBasedViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CodeBasedViewController.swift
+//  LoadViewControllers
+//
+//  Created by diayan siat on 14/03/2022.
+//
+
+import UIKit
+
+class CodeBasedViewController: UIViewController {
+    private let data: String
+    
+    //we don't expect anything to call this initializer ever 
+    init(data: String) {
+        self.data = data
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
